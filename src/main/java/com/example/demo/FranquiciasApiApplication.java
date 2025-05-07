77a6cccb8d2bbfa3c9afee40296af5af4aa7d0bd
dleon <odeleon@arkho.io>
2025-05-07 01:43:12 -0500

    init project

---
+++ src/main/java/com/example/demo/FranquiciasApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranquiciasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranquiciasApiApplication.class, args);
+	}
+
+}
